fd5be8d276a70288e79730b41e67f5cb665e0dae
Shawnsuun <illusionruin@gmail.com>
2023-11-20 23:30:50 -0500

    first commit

---
+++ hello.js
@@ -0,0 +1,13 @@
+//app.get('/hello', (req, res) => {res.send('Life is good!')})
+//app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+
+const Hello = (app) => {
+    app.get('/hello', (req, res) => {
+      res.send('Life is good!')
+    })
+    app.get('/', (req, res) => {
+      res.send('Welcome to Full Stack Development!')
+    })
+  }
+  export default Hello;
+  
