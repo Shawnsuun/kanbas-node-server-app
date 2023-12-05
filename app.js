09c2722022015f4192073aec7bf86778cd505d2f
Shawnsuun <illusionruin@gmail.com>
2023-12-04 22:41:18 -0500

    update env

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ import Lab5 from "./lab5.js";
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 import cors from "cors";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
-mongoose.connect(CONNECTION_STRING);
+
 
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./Modules/routes.js";
 import "dotenv/config";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
 
 const app = express()
 app.use(
