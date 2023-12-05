f71cb6809f554a9899cc4c9f2f30875a7951ee3c
Shawnsuun <illusionruin@gmail.com>
2023-12-04 22:12:10 -0500

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
+import "dotenv/config";
 import express from "express";
+import session from "express-session";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
+
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
 import cors from "cors";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
+
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./Modules/routes.js";
 import "dotenv/config";
 
 const app = express()
 app.use(
-    cors({
-      credentials: true,
-      origin: process.env.FRONTEND_URL
-    })
-  );
-  
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
+
+app.use(express.json());
+UserRoutes(app);
+
 ModuleRoutes(app);
 CourseRoutes(app);
-app.use(express.json());
+
 Lab5(app);
 Hello(app)
 app.listen(process.env.PORT || 4000);
